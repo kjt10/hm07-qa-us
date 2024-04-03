f846334e6b019154aa3c2fd341e7260ff73f8ccd
Kelly <ktrejosa10@gmail.com>
2024-04-02 18:49:55 -0700

    uploading the get handlers tests

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "paste_your_api_url_here"
+    API_URL: "https://cnt-6329687f-8350-48c7-bdf1-8c53eb5be500.containerhub.tripleten-services.com"
 }
--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -1,10 +1,25 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
 
-test('', async () => {
+test('Return 200 OK status code', async () => {
+	let actualStatusCode;
 	try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`);
+		const response = await fetch(`${config.API_URL}/api/v1/warehouses`);
+		actualStatusCode = response.status
 	} catch (error) {
 		console.error(error);
 	}
+	expect(actualStatusCode).toBe(200);
+});
+
+test('Return an array in the response body', async () => {
+	let responseBody;
+	try {
+		const response = await fetch(`${config.API_URL}/api/v1/warehouses`);
+		responseBody = await response.json()
+	} catch (error) {
+		console.error(error);
+	}
+	expect(typeof responseBody).toBe('object');
+	expect(responseBody.length).toBeGreaterThanOrEqual(1);
 });
